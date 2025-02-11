53e1d5318c76d15d37e7381267abaa26513f80be
angelolagahit <lagahit.angelo@gmail.com>
2025-02-11 13:31:20 +0800

    Added logic for each dataframe to include removing duplicate HPO and phenotype terms for each patient.

--- 1. Variant Database/Prototype V5/variant/standardise.py
+++ 1. Variant Database/Prototype V5/variant/standardise.py
@@ -2,6 +2,44 @@ from variant.dataframe import *
 
 
 
+def standardise_lab_cases(dataframes, standard_columns):
+    """
+    Standardises the Lab Cases dataset.
+
+    Parameters:
+    -----------
+    dataframes : dict
+        Dictionary containing all loaded DataFrames.
+    standard_columns : list
+        List of standard column names to retain.
+
+    Returns:
+    --------
+    pd.DataFrame
+        A standardized DataFrame for Lab Cases.
+    """
+    lab_cases_cols = {
+        'G4K Sample ID': 'MRN',
+        'Number variants detected': 'Var Count',
+        'Variant_Number': 'Var Number',
+        'gene': 'Gene',
+        'type': 'Type',
+        'zygosity': 'Zygosity',
+        'inheritance': 'Inheritance'
+    }
+
+    std_lab_cases_df = standardise_and_transform_lab_cases(dataframes["Lab Cases"])
+    std_lab_cases_df.rename(columns=lab_cases_cols, inplace=True)
+    std_lab_cases_df = std_lab_cases_df.loc[:, std_lab_cases_df.columns.intersection(standard_columns)]
+    std_lab_cases_df = std_lab_cases_df.reindex(columns=standard_columns)
+    remove_duplicate_phenotypes(std_lab_cases_df)
+    remove_duplicate_hpo_terms(std_lab_cases_df)
+    std_lab_cases_df = filter_invalid_variants(std_lab_cases_df)
+
+    return std_lab_cases_df
+
+
+
 def standardise_invitae_summary(dataframes, standard_columns):
     """
     Standardises the Invitae Summary dataset.
@@ -29,6 +67,8 @@ def standardise_invitae_summary(dataframes, standard_columns):
     }
 
     standard_dataframe = standardise_dataframe(dataframes["Invitae Summary"], invitae_summary_cols, standard_columns)
+    remove_duplicate_phenotypes(standard_dataframe)
+    remove_duplicate_hpo_terms(standard_dataframe)
     
     return standard_dataframe
 
@@ -57,8 +97,11 @@ def standardise_clinical_summary(dataframes, standard_columns):
     }
 
     standard_dataframe = standardise_dataframe(dataframes["Clinical Summary"], clinical_summary_cols, standard_columns)
+    remove_duplicate_phenotypes(standard_dataframe)
+    remove_duplicate_hpo_terms(standard_dataframe)
     
     return standard_dataframe
+
 
 
 def standardise_research_summary(dataframes, standard_columns):
@@ -79,8 +122,11 @@ def standardise_research_summary(dataframes, standard_columns):
     """
     standard_dataframe = standardise_and_transform_research(dataframes["Research Summary"])
     standard_dataframe = standard_dataframe.loc[:, standard_dataframe.columns.intersection(standard_columns)]
+    standard_dataframe = standard_dataframe.reindex(columns=standard_columns)
+    remove_duplicate_phenotypes(standard_dataframe)
+    remove_duplicate_hpo_terms(standard_dataframe)
     
-    return standard_dataframe.reindex(columns=standard_columns)
+    return standard_dataframe
 
 
 
@@ -102,8 +148,11 @@ def standardise_atm_summary(dataframes, standard_columns):
     """
     standard_dataframe = standardise_and_transform_atm(dataframes["ATM Summary"])
     standard_dataframe = standard_dataframe.loc[:, standard_dataframe.columns.intersection(standard_columns)]
+    standard_dataframe = standard_dataframe.reindex(columns=standard_columns)
+    remove_duplicate_phenotypes(standard_dataframe)
+    remove_duplicate_hpo_terms(standard_dataframe)
     
-    return standard_dataframe.reindex(columns=standard_columns)
+    return standard_dataframe
 
 
 
@@ -159,6 +208,10 @@ def combine_standardised_data(std_dataframes):
         ],
         axis=0, ignore_index=True
     )
+    # Convert float columns to integers
+    numeric_columns = ["Var Number", "Var Count"]  # Add other numeric columns if needed
+    combined_df[numeric_columns] = combined_df[numeric_columns].round(0).astype("Int64")  # Int64 allows NaN values
+    
     return combined_df
 
 
@@ -188,6 +241,7 @@ def standardise_data(dataframes, standard_columns):
     new_df = combine_standardised_data(std_other_datasets)
 
     return new_df
+
 
 
 def generate_hgvs_column(df):
